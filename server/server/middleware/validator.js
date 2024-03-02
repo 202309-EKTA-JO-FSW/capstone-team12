b9a994ed594b8c4ce4b789d660e7262ed4bec560
Hasan <h.mjabai92@gmail.com>
2024-03-03 01:04:39 +0300

    validator

--- server/server/middleware/validator.js
+++ server/server/middleware/validator.js
@@ -1,1 +1,23 @@
-//for password 
+const { check } = require("express-validator");
+
+const validatorUserSigneUp = [
+  check("fullname")
+    .trim()
+    .not()
+    .isEmpty()
+    .isString()
+    .isLength({ min: 3, max: 20 })
+    .withMessage("Name must be within 3 to 20 character"),
+  check("email").normalizeEmail().isEmail().withMessage("Invalide email"),
+  check("password").trim().not().isEmpty().isLength({ min: 3, max: 10 }),
+];
+const validationResult = (req, res, next) => {
+  const result = validationResult(req).array();
+  if (!result.length) return next();
+  const error = result[0].msg;
+  res.json({ seccess: false, message: error });
+};
+
+module.exports = {
+  validatorUserSigneUp,validationResult
+};
